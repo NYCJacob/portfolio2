bf2b3d6e87087b94f9145840382eeb3f2403717c
Jacob Sherman <jacob@jbsherman.com>
2017-11-11 16:51:53 -0500

    Fix: gulp tasks added; not complete

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,10 @@ var gulp = require('gulp');
 // and attach them to the `plugins` object
 var plugins = require('gulp-load-plugins')();
 
-// browser-sync does not seem to load in load-plugins function
 // these plugins do not seem to register with load-plugins
 var concatCss = require('gulp-concat-css');
+var htmlreplace = require('gulp-html-replace');
+
 
 
 // Temporary solution until gulp 4
@@ -29,23 +30,29 @@ gulp.task('lint:sass',	function()	{
 
 
 gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
+    // custom is entry point for custom css and imports
+    return gulp.src('src/scss/custom.scss')
     // pass custom title to gulp-plumber error handler function
         .pipe(customPlumber('Error Running	Sass'))
         //	Initialize	sourcemap
         .pipe(plugins.sourcemaps.init())
         .pipe(plugins.sass())
+        .pipe(concatCss('styles.css'))
         // remove unused css
-        //TODO: .pipe(purify(['./public/app/**/*.js', './public/**/*.html']))
+        .pipe(plugins.uncss({
+            html: ['src/index.html']
+        }))
         //	Runs	produced	CSS	through	autoprefixer
         .pipe(plugins.autoprefixer())
+        // compress css
+        .pipe(plugins.cssnano())
         //	Writing	sourcemaps
         .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest('src/css'));
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/scss/custom.scss', ['sass']);
 });
 
 gulp.task('sass:vendor',	function()	{
@@ -72,6 +79,18 @@ gulp.task('min-css', function() {
     return gulp.src('src/css/*.css', {base: 'src'})
         .pipe(concatCss("css/styles.min.css"))
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist'));
+});
+
+
+// js tasks
+
+
+// html tasks
+gulp.task('html:min', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlreplace({js: 'js/app.js', css: 'css/styles.css'}))
+        // .pipe(plugins.htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 });
 
